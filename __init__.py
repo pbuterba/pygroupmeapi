c5604d0ad35946f488135e21755bdd0b94880a32
pbuterba <pbuterba.corp@gmail.com>
2024-07-27 14:21:58 -0400

    Export Message class

--- __init__.py
+++ __init__.py
@@ -3,10 +3,13 @@ from groupme.base_object import GroupMe
 # noinspection PyUnresolvedReferences
 from groupme.chat import Chat, Group, DirectMessage
 # noinspection PyUnresolvedReferences
+from groupme.message import Message
+# noinspection PyUnresolvedReferences
 from groupme.common_utils import GroupMeException
 
 GroupMe = GroupMe
 Chat = Chat
 Group = Group
 DirectMessage = DirectMessage
+Message = Message
 GroupMeException = GroupMeException
