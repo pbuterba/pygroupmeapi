db06b390797df7b0f6455df1e69fe2c7d9b99661
pbuterba <pbuterba.corp@gmail.com>
2024-07-27 21:44:53 -0400

    Remove linter instructions

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
-# noinspection PyUnresolvedReferences
 from groupme.base_object import GroupMe
-# noinspection PyUnresolvedReferences
 from groupme.chat import Chat, Group, DirectMessage
-# noinspection PyUnresolvedReferences
 from groupme.message import Message
-# noinspection PyUnresolvedReferences
 from groupme.common_utils import GroupMeException
 
 GroupMe = GroupMe
